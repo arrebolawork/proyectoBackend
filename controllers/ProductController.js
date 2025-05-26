94fbe38455886d0c271b2367c07120d9199a2c40
David Arrebola <arrebola.work@gmail.com>
2025-05-26 19:46:22 +0200

    endpoint para actualizar con ruta

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -15,6 +15,27 @@ const ProductController = {
       .then((product) => res.status(201).send({ message: "Producto creado con éxito", product }))
       .catch((err) => console.error(err));
   },
+  updateProduct(req, res) {
+    const { id } = req.params;
+
+    Product.update(req.body, { where: { id } })
+      .then(([updated]) => {
+        if (updated) {
+          return Product.findByPk(id);
+        } else {
+          res.status(404).send({ message: "Producto no encontrado" });
+        }
+      })
+      .then((updatedProduct) => {
+        if (updatedProduct) {
+          res.status(200).send({ message: "Producto actualizado con éxito", product: updatedProduct });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send({ message: "Error al actualizar el producto" });
+      });
+  },
 };
 module.exports = ProductController;
 >>>>>>> 64384d0 (endpoint para crear producto con ruta)
