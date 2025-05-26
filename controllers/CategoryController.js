7187aa4be0b234efee2b01dca878a5730769da54
eduardolm1 <eduardolm300601@gmail.com>
2025-05-26 19:55:01 +0200

    Implementando usuarios, order, product, category(controladores modelos)

---
+++ controllers/CategoryController.js
@@ -0,0 +1,7 @@
+const { Category } = require('../models/index')
+
+const CategoryController = {
+
+}
+
+module.exports = CategoryController
---
+++ models/orderproduct.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class OrderProduct extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  OrderProduct.init({
+    ProductId: DataTypes.INTEGER,
+    OrderId: DataTypes.INTEGER,
+    productCount: DataTypes.INTEGER,
+    totalPrice: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'OrderProduct',
+  });
+  return OrderProduct;
+};
