841ee96a3b678aeceecc7517a002ac5a4d42e8c5
David Arrebola <arrebola.work@gmail.com>
2025-05-26 19:35:30 +0200

    endpoint para crear producto con ruta

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const { Product } = require('../models/index')
 
 const ProductController = {
@@ -5,3 +6,15 @@ const ProductController = {
 }
 
 module.exports = ProductController
+=======
+//no se si falta la conexion a la base de datos???
+const { Product } = require("../models/index.js");
+const ProductController = {
+  createProduct(req, res) {
+    Product.create(req.body)
+      .then((product) => res.status(201).send({ message: "Producto creado con éxito", product }))
+      .catch((err) => console.error(err));
+  },
+};
+module.exports = ProductController;
+>>>>>>> 64384d0 (endpoint para crear producto con ruta)
