4837701e04ebee614b65e071fb275b85e281840b
David Arrebola <arrebola.work@gmail.com>
2025-05-26 20:09:58 +0200

    ruta para el endpoint de precio

--- routes/products.js
+++ routes/products.js
@@ -7,5 +7,6 @@ router.put("/:id", productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
 router.get("/:id", productController.getById);
 router.get("/search", productController.getProductByName);
+router.get("/price", productController.getProductByPrice);
 
 module.exports = router;
