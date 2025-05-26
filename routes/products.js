3b909204847cd40867778ac2c562452d6e975b78
David Arrebola <arrebola.work@gmail.com>
2025-05-26 19:53:50 +0200

    endpoint para mostrar un prodducto por id

--- routes/products.js
+++ routes/products.js
@@ -5,5 +5,6 @@ const productController = require("../controllers/ProductController");
 router.post("/", productController.createProduct);
 router.put("/:id", productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
+router.get("/:id", productController.getById);
 
 module.exports = router;
