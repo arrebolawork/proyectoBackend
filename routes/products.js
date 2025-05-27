48f1f4f24d148a1f079394ee8cb0ebee540f95d7
David Arrebola <arrebola.work@gmail.com>
2025-05-27 18:38:19 +0200

    Creada la validacion de producto para que pida los datos o muestre mensaje

--- routes/products.js
+++ routes/products.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const productController = require("../controllers/ProductController");
 
 router.post("/", productController.createProduct);
-router.put("/:id", productController.updateProduct);
-router.delete("/:id", productController.deleteProduct);
-router.get("/:id", productController.getById);
 router.get("/search", productController.getProductByName);
 router.get("/price", productController.getProductByPrice);
+router.get("/:id", productController.getById);
+router.put("/:id", productController.updateProduct);
+router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;
