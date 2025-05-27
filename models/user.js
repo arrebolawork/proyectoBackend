7e84706609aac066e9191631ef2ec44d0de525fb
David Arrebola <arrebola.work@gmail.com>
2025-05-27 19:20:29 +0200

    bcrypt y JWT para user listo. creados los dos endpoint para registro y login

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const bcrypt = require("bcrypt");
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -10,17 +10,36 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(models.Order)
+      User.hasMany(models.Order);
+    }
+    async validatePassword(password) {
+      return await bcrypt.compare(password, this.passwd);
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    passwd: DataTypes.STRING,
-    birthday: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'User',
+  User.init(
+    {
+      name: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      passwd: DataTypes.STRING,
+      birthday: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  User.beforeCreate(async (user, options) => {
+    if (user.passwd) {
+      const salt = await bcrypt.genSalt(10);
+      user.passwd = await bcrypt.hash(user.passwd, salt);
+    }
   });
+  User.beforeUpdate(async (user, options) => {
+    if (user.changed("passwd")) {
+      const salt = await bcrypt.genSalt(10);
+      user.passwd = await bcrypt.hash(user.passwd, salt);
+    }
+  });
+
   return User;
 };
