ace84ab598ddfbbc0453d3179bb9d84b86d066b2
David Arrebola <arrebola.work@gmail.com>
2025-05-30 18:49:24 +0200

    validaciones en los modelos. token y authorizacion en logout

--- models/product.js
+++ models/product.js
@@ -1,23 +1,37 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
-    
     static associate(models) {
       Product.belongsToMany(models.Order, {
         through: models.OrderProduct,
-        foreignKey: 'ProductId',
-        otherKey: 'OrderId'
+        foreignKey: "ProductId",
+        otherKey: "OrderId",
       });
-      Product.belongsTo(models.Category, { foreignKey: 'categoryId' });
+      Product.belongsTo(models.Category, { foreignKey: "categoryId" });
     }
   }
-  Product.init({
-    name: DataTypes.STRING,
-    price: DataTypes.FLOAT
-  }, {
-    sequelize,
-    modelName: 'Product',
-  });
+  Product.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Por favor introduce el nombre" },
+        },
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Por favor introduce el nombre" },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Product",
+    }
+  );
   return Product;
 };
