1d0529c3aaf732c99598364b8ad5207cb3653581
David Arrebola <arrebola.work@gmail.com>
2025-05-31 09:33:28 +0200

    corregido el errors del token

--- models/token.js
+++ models/token.js
@@ -1,5 +1,7 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Token extends Model {
     /**
@@ -8,18 +10,15 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Token.belongsTo(models.User);
+      // define association here
     }
   }
-  Token.init(
-    {
-      token: DataTypes.STRING,
-      UserId: DataTypes.INTEGER,
-    },
-    {
-      sequelize,
-      modelName: "Token",
-    }
-  );
+  Token.init({
+    token: DataTypes.STRING,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Token',
+  });
   return Token;
 };
